704c5bc00c3176492242567946a4ad9c161efdc0
Zeeshan <zeeshan.zahoor@toptal.com>
2022-05-13 12:52:01 +0300

    modal design

--- components/modal/component.stories.tsx
+++ components/modal/component.stories.tsx
@@ -2,9 +2,8 @@ import { useState } from 'react';
 
 import { Story } from '@storybook/react/types-6-0';
 
-import Button from 'components/button';
-
 import Modal, { ModalProps } from './index';
+import Button from 'components/button/component';
 
 export default {
   title: 'Components/Modal',
@@ -40,14 +39,23 @@ const Template: Story<ModalProps> = ({ ...args }: ModalProps) => {
 export const Default: Story<ModalProps> = Template.bind({});
 Default.args = {
   title: 'Modal component',
-  dismissable: true,
+  dismissable: false,
   children: (
-    <div className="px-10">
-      <h1 className="mb-5 text-xl font-medium">Modal content</h1>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at sodales est, eu imperdiet
-      elit. Suspendisse eget diam accumsan, lacinia odio nec, fringilla ex. Quisque consectetur diam
-      in massa egestas, vitae posuere magna semper. Sed ac iaculis purus, at pretium tellus. Duis
-      non commodo lorem, non tincidunt ex.
+    <div className='relative h-full'>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at sodales est, eu imperdiet
+        elit. Suspendisse eget diam accumsan, lacinia odio nec, fringilla ex. Quisque consectetur
+        diam in massa egestas, vitae posuere magna semper. Sed ac iaculis purus, at pretium tellus.
+        Duis non commodo lorem, non tincidunt ex.
+      </div>
+      <div className='flex absolute bottom-0'>
+        <div className="w-[80px] relative mr-2">
+          <Button theme="primary" cut='right-bottom'>View</Button>
+        </div>
+        <div className="w-[80px] relative">
+          <Button theme="primary">Download</Button>
+        </div>
+      </div>
     </div>
   ),
 };
--- components/modal/constants.ts
+++ components/modal/constants.ts
@@ -1,9 +1,9 @@
 export const COMMON_CONTENT_CLASSES =
-  'absolute top-1/2 inset-x-4 sm:left-1/2 transform -translate-y-1/2 sm:-translate-x-1/2 outline-none bg-white flex flex-col flex-grow overflow-hidden rounded-3xl py-7';
+  'absolute top-1/2 inset-x-4 sm:left-1/2 transform -translate-y-1/2 sm:-translate-x-1/2 bg-blur border border-mainblue p-3 w-[250px] h-[400px]';
 export const CONTENT_CLASSES = {
-  narrow: `sm:w-4/6 md:w-1/2 lg:w-5/12 xl:w-1/3 ${COMMON_CONTENT_CLASSES}`,
-  default: `sm:w-4/5 md:w-2/3 lg:1/2 xl:w-2/5 ${COMMON_CONTENT_CLASSES}`,
-  wide: `sm:w-10/12 md:w-10/12 lg:w-10/12 xl:w-8/12 ${COMMON_CONTENT_CLASSES}`,
+  narrow: ` ${COMMON_CONTENT_CLASSES}`,
+  default: ` ${COMMON_CONTENT_CLASSES}`,
+  wide: ` ${COMMON_CONTENT_CLASSES}`,
 };
 
 export const OVERLAY_CLASSES = 'z-50 fixed inset-0 bg-black bg-blur';
